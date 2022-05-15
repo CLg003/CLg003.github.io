e0198daed3738e0265a805cd5622e4af5cdeb783
CLg003 <clairemjlaing@gmail.com>
2022-05-15 22:12:37 +0100

    Updated project descriptions and index.html file.

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
     const projects = [
         {name: "Pulse Gym Management", 
         type: "Python full-stack web app", 
-        description: "Project description here. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ", 
+        description: "Python full-stack web app, designed and built as a solo project, that enables gym staff to manage memberships, classes and bookings - built over 5 days using Python, Flask & PostgreSQL.", 
         image: "/assets/images/project_videos_and_screenshots/pulse_ss/pulse_home_page_ss.png", 
         detailImages: [
             "/assets/images/project_videos_and_screenshots/pulse_ss/pulse_home_page_ss.png", 
@@ -20,7 +20,7 @@ const Projects = () => {
         },
         {name: "NBA 75", 
         type: "React/REST API full-stack web app", 
-        description: "Project description here.", 
+        description: "React web app, designed and built as a solo assignment, that uses data from an external REST API to display information about the NBA's 75th Anniversary Greatest Players of All Time - built over 2 days using JavaScript, React and an external REST API. Please note that, due to request limits on the API, I had to amend the remit of this app to show the 25 greatest NBS players of all time!", 
         image: "/assets/images/project_videos_and_screenshots/nba_ss/nba_main_ss.png", 
         detailImages: [
             "/assets/images/project_videos_and_screenshots/nba_ss/nba_main_ss.png", 
@@ -34,7 +34,7 @@ const Projects = () => {
         },
         {name: "Triolingo", 
         type: "React full-stack web app", 
-        description: "Project description here.", 
+        description: "React full-stack educational web app, designed and built as a group project, to teach Spanish to primary-aged children - built over 5 days using JavaScript, React, Express & MongoDB.", 
         image: "/assets/images/project_videos_and_screenshots/triolingo_ss/triolingo_lesson2_reset_ss.png", 
         detailImages: [
             "/assets/images/project_videos_and_screenshots/triolingo_ss/triolingo_landing_screen_ss.png", 
